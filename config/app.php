4e7759b547a775fabbd2b1e5434f3bd79e3254c3
neilaldrei <neilaldreiii@gmail.com>
2019-04-27 13:56:17 +0800

    Finished Maatwebsite(Excel file handler) Setup

--- config/app.php
+++ config/app.php
@@ -171,9 +171,12 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -225,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class
 
     ],
 
