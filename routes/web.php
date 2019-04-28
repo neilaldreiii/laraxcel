783a67b733a53ebcbbd768bee48dcfec4c167951
neilaldrei <neilaldreiii@gmail.com>
2019-04-28 13:14:49 +0800

    Finished Import Export Excel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('export', 'MyController@export')->name('export');
+Route::get('importExportView', 'MyController@importExportView');
+Route::post('import', 'MyController@import')->name('import');
